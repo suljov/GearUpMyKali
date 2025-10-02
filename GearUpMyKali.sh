8b7ee99b3c1b553042dd8efd321453cdce3d4634
Suljov <albin.suljov@hotmail.com>
2025-10-02 10:49:01 +0200

    Update GearUpMyKali.sh

--- GearUpMyKali.sh
+++ GearUpMyKali.sh
@@ -339,6 +339,8 @@ repo_urls=(
     "https://github.com/BloodHoundAD/BARK"
     "https://github.com/aancw/SSLPinDetect"
     "https://github.com/carlospolop/cloudpeass"
+    "https://github.com/cobbr/SharpSploit"
+    "https://github.com/olafhartong/Invoke-Phant0m"
 )
 
 # Loop through the repository URLs and clone each one into the current directory
@@ -348,7 +350,7 @@ for url in "${repo_urls[@]}"; do
     git clone "$url"
 done
 
-mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester ntlm_theft windapsearch Rubeus PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
+mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester ntlm_theft windapsearch Rubeus PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx SharpSploit Invoke-Phant0m impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
 mv linux-exploit-suggester linux-exploit-suggester-2 LinEnum linuxprivchecker system/linux/
 mv Pentest-Cheatsheets misc/
 mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray o365enum GraphRunner basicblobfinder Graphpython BARK cloud/Azure
