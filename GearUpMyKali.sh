69f0989051ae852d4328264de499cb6d1011b425
Suljov <albin.suljov@hotmail.com>
2025-11-03 09:29:07 +0100

    Update repository URLs and move directories

--- GearUpMyKali.sh
+++ GearUpMyKali.sh
@@ -342,6 +342,7 @@ repo_urls=(
     "https://github.com/p0dalirius/Coercer"
     "https://github.com/stealthcopter/deepce"
     "https://github.com/mordavid/NetworkHound"
+    "https://github.com/adrecon/ADRecon"
 )
 
 # Loop through the repository URLs and clone each one into the current directory
@@ -351,7 +352,7 @@ for url in "${repo_urls[@]}"; do
     git clone "$url"
 done
 
-mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester PortBender winpspy StreamDivert Coercer ntlm_theft windapsearch Rubeus Whisker SharpChromium HackBrowserData PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx SharpSploit Invoke-Phant0m impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
+mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester PortBender winpspy ADRecon StreamDivert Coercer ntlm_theft windapsearch Rubeus Whisker SharpChromium HackBrowserData PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx SharpSploit Invoke-Phant0m impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
 mv linux-exploit-suggester linux-exploit-suggester-2 LinEnum linuxprivchecker system/linux/
 mv Pentest-Cheatsheets search_vulns misc/
 mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray o365enum GraphRunner basicblobfinder Graphpython BARK cloud/Azure
