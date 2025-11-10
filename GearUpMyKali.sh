ff7db925223d59769a960c0c54e0f09721066131
Suljov <albin.suljov@hotmail.com>
2025-11-10 11:27:29 +0100

    Update GearUpMyKali.sh

--- GearUpMyKali.sh
+++ GearUpMyKali.sh
@@ -347,6 +347,7 @@ repo_urls=(
     "https://github.com/Hackndo/pyGPOAbuse"
     "https://github.com/000pp/tomcter"
     "https://github.com/ptoomey3/evilarc"
+    "https://github.com/Yaxxine7/ASRepCatcher"
 )
 
 # Loop through the repository URLs and clone each one into the current directory
@@ -356,7 +357,7 @@ for url in "${repo_urls[@]}"; do
     git clone "$url"
 done
 
-mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester PortBender winpspy ADRecon StreamDivert pyGPOAbuse ADCollector Coercer ntlm_theft windapsearch Rubeus Whisker SharpChromium HackBrowserData PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 Seatbelt bloodyAD krbrelayx SharpSploit Invoke-Phant0m impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
+mv Ghostpack-CompiledBinaries Inveigh Windows-Exploit-Suggester PortBender winpspy ADRecon StreamDivert pyGPOAbuse ADCollector Coercer ntlm_theft windapsearch Rubeus Whisker SharpChromium HackBrowserData PrivescCheck mimikatz nc.exe PetitPotam SharpUp CVE-2023-2255 ASRepCatcher Seatbelt bloodyAD krbrelayx SharpSploit Invoke-Phant0m impacket PrecompiledBinaries SharpCollection BloodHound.py adPEAS windowsprivchecker BloodHound donut powersploit system/windows/
 mv linux-exploit-suggester linux-exploit-suggester-2 LinEnum linuxprivchecker system/linux/
 mv Pentest-Cheatsheets search_vulns evilarc misc/
 mv TokenTacticsV2 AzSubEnum Oh365UserFinder o365spray o365enum GraphRunner basicblobfinder Graphpython BARK cloud/Azure
